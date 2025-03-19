7a0e4cb968dea610c12bdeb951043b41ced2593d
jmtomeX <jmtome00@gmail.com>
2025-03-19 10:55:36 +0100

    Añadida tabla piloto

--- form-matia/src/App.tsx
+++ form-matia/src/App.tsx
@@ -1,6 +1,5 @@
+import ClientTable from "./components/ClientTable";
 import FormClient from "./components/FormClient";
-
-// Definición de tipos
 
 const App = () => {
   return (
@@ -23,37 +22,7 @@ const App = () => {
       <main className="flex justify-center flex-col mt-20 p-10">
         <FormClient />
         <div className="mt-10">
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi,
-            vero, quo unde et possimus ipsam iure, cum quam repudiandae magnam
-            eius. Repellat velit distinctio molestiae rerum qui quasi sint
-            facilis? Quam asperiores natus dolor ullam distinctio. Vitae minima
-            voluptate at? Doloremque minima commodi officia quo facere,
-            asperiores sequi reiciendis blanditiis vel esse? Explicabo, ipsa
-            error tenetur assumenda quae odit ducimus. Id corrupti porro cum
-            deleniti doloremque tempora a rem repellat eos adipisci. Iusto
-            impedit fugiat architecto. Minima nostrum voluptas sed ullam
-            consectetur odio tempore asperiores necessitatibus, harum voluptatem
-            molestias error? Sit, perferendis in provident quos ex aspernatur ab
-            perspiciatis nesciunt odit iusto, impedit enim nostrum labore quidem
-            recusandae minus excepturi veniam mollitia nam repellat autem.
-            Placeat excepturi dicta repudiandae expedita. Hic quos alias
-            perferendis culpa excepturi, architecto aliquid sapiente nemo autem
-            aliquam aspernatur nulla optio consequatur exercitationem eligendi
-            nisi a pariatur eos illum aperiam maiores itaque nihil illo.
-            Blanditiis, nostrum. Vitae quasi omnis amet mollitia est beatae!
-            Minus voluptas inventore numquam fuga voluptatem amet repellat
-            accusantium tenetur odio obcaecati? Incidunt cumque, molestiae enim
-            nulla nobis rerum earum inventore dignissimos sapiente! Optio
-            voluptatem similique eum doloribus atque, accusantium, sint iste,
-            mollitia eos quos asperiores rem nesciunt reiciendis eaque est
-            explicabo sunt. Libero deserunt error harum beatae doloribus? Ex
-            libero dolor ullam. Iste odio labore optio mollitia quidem sapiente
-            rem cupiditate beatae, officiis dolorem perspiciatis, consectetur
-            vel, similique alias blanditiis officia? Mollitia, magnam
-            praesentium molestias illum voluptatum impedit cupiditate? Culpa,
-            perspiciatis explicabo!
-          </p>
+          <ClientTable />
         </div>
       </main>
     </>
---
+++ form-matia/src/components/ClientTable.tsx
@@ -0,0 +1,66 @@
+
+function ClientTable() {
+  return (
+    <div className="w-full max-w-6xl mx-auto bg-white shadow-lg rounded-lg overflow-hidden">
+      <div className="bg-gradient-to-r from-pink-900 to-pink-500 px-6 p-4">
+        <h2 className="text-2xl font-bold text-white">
+          Contactos añadidos
+        </h2>
+      </div>
+      <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+          <tr>
+            <th scope="col" className="px-6 py-3">
+              Nombre
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Apellido
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Teléfono
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Email
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Pais
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Provicia
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
+            <th
+              scope="row"
+              className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+            >
+              Apple MacBook Pro 17"
+            </th>
+            <td className="px-6 py-4">Silver</td>
+            <td className="px-6 py-4">Laptop</td>
+            <td className="px-6 py-4">$2999</td>
+            <td className="px-6 py-4"></td>
+          </tr>
+          <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
+            <th
+              scope="row"
+              className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+            >
+              Microsoft Surface Pro
+            </th>
+            <td className="px-6 py-4">White</td>
+            <td className="px-6 py-4">Laptop PC</td>
+            <td className="px-6 py-4">$1999</td>
+            <td className="px-6 py-4"></td>           
+            <td className="px-6 py-4"></td>           
+         
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default ClientTable;
