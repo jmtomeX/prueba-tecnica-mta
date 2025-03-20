ec59dd611af84fc7cda9970f8b72b015ea92f4bf
jmtomeX <jmtome00@gmail.com>
2025-03-20 09:20:16 +0100

    Eliminado dato iso3 recibido de la petición a la API

--- src/apis/countries-api.ts
+++ src/apis/countries-api.ts
@@ -3,7 +3,6 @@ import { isAxiosError } from "axios";
 
 export interface Countries {
     name: string;
-    iso3: string;
 }
 export interface States {
     name: string;
@@ -18,7 +17,7 @@ export const getCountries = async () => {
         const response = await api.get(url);
         const { data: { data } } = response;
 
-        const countries = data.map(({ name, iso3 }: Countries) => ({ name, iso3 }));
+        const countries = data.map(({ name }: Countries) => ({ name }));
 
         return countries;
     } catch (error) {
